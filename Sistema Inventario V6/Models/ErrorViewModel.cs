31362a96c94df1cedf3204cdf17e4a12c7673b4b
sergcode <sergiovelasquezn@gmail.com>
2023-04-27 16:15:50 -0400

    Configuracion del Proyecto

--- Sistema Inventario V6/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sistema_Inventario_V6.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage ="Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser Maximo 60 caracteres")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es Requerida")]
+        [MaxLength(100, ErrorMessage = "Descripcion debe ser Maximo 100 caracteres")]
+        public  String Descripcion { get; set; }
+
+        [Required(ErrorMessage ="El Estado es Requerido")]
+        public bool Estado { get; set; }
+    }
+}
